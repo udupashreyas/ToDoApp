69d7461d163838be99440bb2f89e80cff185517b
udupashreyas <udupa_shreyas@yahoo.co.in>
2016-11-23 23:04:59 -0800

    added authentication

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -12,11 +12,11 @@ import hello.views
 urlpatterns = [
     #url(r'^$', hello.views.index, name='index'),
     url(r'^$', 'django.contrib.auth.views.login'),
-    #url(r'^logout/$', logout_page),
+    url(r'^logout/$', hello.views.logout_page, name='logout_page'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
-    url(r'^register/$', register),
-    url(r'^register/success/$', register_success),
-    url(r'^home/$', home),
+    url(r'^register/$', hello.views.register, name='register'),
+    url(r'^register/success/$', hello.views.register_success, name='register_success'),
+    url(r'^home/$', hello.views.home, name='home'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^status', hello.views.status_report, name='status_report'),
     url(r'^admin/', include(admin.site.urls)),
