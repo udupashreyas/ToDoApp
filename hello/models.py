0cc547b02eb1cf4a9f5e0012aa8b06b70577302a
udupashreyas <udupa_shreyas@yahoo.co.in>
2016-11-25 00:54:22 -0800

    added foreign key

--- hello/models.py
+++ hello/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import datetime
 
 PRIORITY_CHOICES = ((1,'Low'),(2,'Normal'),(3,'High'))
@@ -8,6 +9,7 @@ class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
     
 class List(models.Model):
+	user = models.ForeignKey(User)
 	title = models.CharField(max_length=250, unique=True) 
 	def __str__(self):
 		return self.title 
